e4a033922ea1236da89445ef4f8f89bcff616351
ground-cinnamon <morkovog@gmail.com>
2024-03-25 21:52:56 +0300

    feat: resolve tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -225,8 +225,10 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const firstDayOfYear = new Date(date.getUTCFullYear(), 0, 1);
+  const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
+  return Math.ceil((pastDaysOfYear + firstDayOfYear.getUTCDay() + 1) / 7);
 }
 
 /**
